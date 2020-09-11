89c31d0be7fefe2e215c40a42a4f97515f323f2d
Julia-Susser <you@example.com>
2020-09-10 22:55:38 -0700

    initial commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.post('/webhook', (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
 
-
+  console.log("WOOHOO")
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
